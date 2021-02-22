b00f5b481fa780d5515138cc8aa92d730ea8647d
brendasutara <sutarabrenda@gamil.com>
2021-02-22 13:01:05 -0300

    Tarea terminada, como costÃó

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -96,12 +96,11 @@ function promedioResultadosTest(resultadosTest) {
   // "resultadosTest" debe ser una matriz de enteros (int/integers)
   // Itera (en un bucle) los elementos del array, calcula y devuelve el promedio de puntajes
   // Tu código:
-var suma =[];
+var suma =0;
 for (var i=0; i<resultadosTest.length;i++){
   suma = suma + resultadosTest[i];
-  total= suma/resultadosTest.length;
 }
-return total;
+return suma/resultadosTest.length;
 }
 
 
@@ -188,7 +187,7 @@ function mesesDelAño(array) {
   //Si alguno de los meses no está, devolver: "No se encontraron los meses pedidos"
   // Tu código:
   var buscados= [];
-  for (var i=0; i= array.length; i++){
+  for (var i=0; i<array.length; i++){
     if (array[i]== 'Enero' || array[i]== 'Marzo' || array[i]== 'Noviembre'){
       buscados.push (array[i]);
     }
@@ -206,6 +205,13 @@ function mayorACien(array) {
   //La función recibe un array con enteros entre 0 y 200. Recorrer el array y guardar en un nuevo array sólo los
   //valores mayores a 100 (no incluye el 100). Finalmente devolver el nuevo array.
   // Tu código:
+ var mas100= [];
+ for (var i=0; i<array.length; i++){
+   if (array[i]>100){
+     mas100.push (array[i]);
+   }
+ }
+ return mas100;
 }
 
 
@@ -217,6 +223,21 @@ function breakStatement(numero) {
   //devolver: "Se interrumpió la ejecución"
   //Pista: usá el statement 'break'
   // Tu código:
+  var array = [];
+  var suma = numero;
+  for(var i= 0; i<10; i++) {
+    suma = suma + 2;
+    if(suma === i) break;
+    else {
+      array.push(suma);
+    }
+  }
+  if(i < 10) {
+    return 'Se interrumpió la ejecución';
+  }
+  else {
+      return array;
+  }
 }
 
 
@@ -227,6 +248,16 @@ function continueStatement(numero) {
   //Cuando el número de iteraciones alcance el valor 5, no se suma en ese caso y se continua con la siguiente iteración
   //Pista: usá el statement 'continue'
   // Tu código:
+  var array = [];
+  var suma = numero;
+  for(var i= 0; i<10; i++) {
+    if(i === 5) continue;
+    else {
+      suma = suma + 2;
+      array.push(suma);
+    }
+  }
+  return array;
 }
 
 
