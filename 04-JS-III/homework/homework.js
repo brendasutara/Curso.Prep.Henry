6e926dc7f30c181fb881008b6ed31022afc87ffd
brendasutara <sutarabrenda@gamil.com>
2021-02-22 09:56:55 -0300

    Faltan poquitos 22 de febrero

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -124,13 +124,27 @@ function multiplicarArgumentos() {
   // Usa la palabra clave `arguments` para multiplicar todos los argumentos y devolver el producto
   // Si no se pasan argumentos devuelve 0. Si se pasa un argumento, simplemente devuélvelo
   // Escribe tu código aquí:
+  if (arguments.length <1){
+    return 0;
+  }
+  var total= 1;
+  for (var i=0; i< arguments.length; i++){
+    total = total * arguments [i];
+  }
+  return total;
 }
 
 
 function cuentoElementos(arreglo){
   //Realiza una función que retorne la cantidad de los elementos del arreglo cuyo valor es mayor a 19.
   //Escribe tu código aquí
-
+  var filtrado= 0;
+  for (var i=0; i<arreglo.length; i++){
+    if (arreglo [i]>19){
+      filtrado ++
+    }
+  }
+ return filtrado
 }
 
 
@@ -139,15 +153,19 @@ function diaDeLaSemana(numeroDeDia) {
   //Realiza una función que dado el número del día de la semana, retorne: Es fin de semana
   //si el día corresponde a Sábado o Domingo y “Es dia Laboral” en caso contrario. 
   //Escribe tu código aquí   
-  
-} 
+  if (numeroDeDia ===1 || numeroDeDia===7){
+      return 'Es fin de semana';
+    }
+      return 'Es dia Laboral';
+    }
 
 
 function empiezaConNueve(n) {
   //Desarrolle una función que recibe como parámetro un número entero n. Debe retornar true si el entero 
   //inicia con 9 y false en otro caso.
   //Escribe tu código aquí
-  
+  var resultado= /^9/.test(n);
+  return resultado;
 }
 
 
@@ -155,7 +173,12 @@ function todosIguales(arreglo) {
   //Escriba la función todosIguales, que indique si todos los elementos de un arreglo son iguales:
   //retornar true, caso contrario retornar false.
   //Escribe tu código aquí  
-  
+  for (var i=0; i<arreglo.length; i++){
+    if (arreglo [i] !== arreglo[i+1]){
+      return false;
+    }
+    return true;
+  }
 } 
 
 
@@ -164,6 +187,18 @@ function mesesDelAño(array) {
   // "Enero", "Marzo" y "Noviembre", guardarlo en nuevo array y retornarlo.
   //Si alguno de los meses no está, devolver: "No se encontraron los meses pedidos"
   // Tu código:
+  var buscados= [];
+  for (var i=0; i= array.length; i++){
+    if (array[i]== 'Enero' || array[i]== 'Marzo' || array[i]== 'Noviembre'){
+      buscados.push (array[i]);
+    }
+ }
+    if (buscados.length<3){
+      return 'No se encontraron los meses pedidos';
+  }
+  else{
+    return buscados;
+  }
 }
 
 
